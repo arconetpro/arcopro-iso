606e586a8ade7732ae8bf5e4d3b5b32e8ce47b5f
arcolinuxz <arcolinuxinfo@gmail.com>
2024-04-12 10:39:01 +0200

    Out

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -36,7 +36,7 @@ echo
 	# setting of the general parameters
 	archisoRequiredVersion="archiso 76-1"
 	buildFolder=$HOME"/arcopro-build"
-	outFolder=$HOME"/arcopro-out"
+	outFolder=$HOME"/arcopro-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
 
 	# If you want to add packages from the chaotics-aur repo then
