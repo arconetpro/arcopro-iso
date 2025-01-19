1df23de98e143ea907b8e20102dec1abbcc89c18
arcolinuxz <arcolinuxinfo@gmail.com>
2025-01-19 07:15:05 +0100

    after creation v25.01.05

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="arcopro"
-iso_label="arcopro-v25.01.05"
+iso_label="arcopro-v25.02.04"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v25.01.05"
+iso_version="v25.02.04"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
